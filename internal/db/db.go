9cfa9be00c5ca0015a11b089e305cc91f6b0d0b7
romandots <ram.d.kreiz@gmail.com>
2025-06-04 06:37:01 +0300

    Add Goose migrations

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,7 +2,14 @@ package db
 
 import (
 	"database/sql"
+	"embed"
+	"errors"
+
+	"github.com/pressly/goose/v3"
 )
+
+//go:embed migrations/*.sql
+var migrationsFS embed.FS
 
 // Connect открывает соединение с Postgres
 func Connect(databaseURL string) (*sql.DB, error) {
@@ -10,6 +17,14 @@ func Connect(databaseURL string) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	goose.SetBaseFS(migrationsFS)
+	goose.SetDialect("postgres")
+	if err := goose.Up(db, "migrations"); err != nil && !errors.Is(err, goose.ErrNoNextVersion) {
+		db.Close()
+		return nil, err
+	}
+
 	// Здесь можно настроить пул, ping и т.д.
 	return db, nil
 }
